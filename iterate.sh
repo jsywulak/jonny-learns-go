ce16dbafac7208ea5d1747b542494a8b3d2bb966
jonny sywulak <jonny@stelligent.com>
2019-11-14 13:17:38 -0500

    iterate 20191114131738

--- iterate.sh
+++ iterate.sh
@@ -1,3 +1,9 @@
-go test -v || git checkout .
-git add . && git commit -m "iterate `date +%Y%m%d%H%M%S`"
-git push -q &
+#!/bin/bash
+
+if go test -v
+then
+    git add . && git commit -m "iterate `date +%Y%m%d%H%M%S`"
+    git push -q &
+else
+    git checkout .
+fi
