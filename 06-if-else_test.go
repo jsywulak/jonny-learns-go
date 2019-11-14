6e9e7cc64c624e4086e55964b605c43a1a9f8715
jonny sywulak <jonny@stelligent.com>
2019-11-14 12:42:09 -0500

    iterate `date +%Y%m%d%H%M%S`

--- 06-if-else_test.go
+++ 06-if-else_test.go
@@ -18,8 +18,6 @@ func TestIfElse(t *testing.T) {
 		assert.Equal(t, 0, 8%4)
 	}
 
-	assert.True(t, true, "true is false?")
-
 	if num := 9; num < 0 {
 		assert.Equal(t, true, num == 0)
 	} else if num < 10 {
