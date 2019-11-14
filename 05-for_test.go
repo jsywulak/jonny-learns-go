83e836164761039bdf32c4150259d9e64c964ead
jonny sywulak <jonny@stelligent.com>
2019-11-14 12:27:17 -0500

    iterate 20191114122717

--- 05-for_test.go
+++ 05-for_test.go
@@ -17,12 +17,10 @@ func TestFor(t *testing.T) {
 	for j := 7; j <= 9; j++ {
 		fmt.Println(j)
 	}
-	fmt.Println("---")
 	for {
 		fmt.Println("loop")
 		break
 	}
-	fmt.Println("---")
 	for n := 0; n <= 5; n++ {
 		if n%2 == 0 {
 			continue
