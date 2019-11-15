10fe5299e5dd5ef9369f6df2920313a601ac8ada
jonny sywulak <jonny@stelligent.com>
2019-11-15 14:34:07 -0500

    iterate 20191115143407

--- 09-slices_test.go
+++ 09-slices_test.go
@@ -16,8 +16,9 @@ func TestSlices(t *testing.T) {
 	s[2] = "c"
 
 	fmt.Println("set:", s)
+	assert.ElementsMatch(t, s, [3]string{"a", "b", "c"})
 	fmt.Println("get:", s[2])
-
+	// asset.Equal(t, "c", s[2])
 	fmt.Println("len:", len(s))
 
 	s = append(s, "d")
