27386792be3cca2e829d6a68289a3d6ce87bf78e
jonny sywulak <jonny@stelligent.com>
2019-11-15 14:25:27 -0500

    iterate 20191115142527

--- 08-arrays_test.go
+++ 08-arrays_test.go
@@ -26,6 +26,6 @@ func TestArrays(t *testing.T) {
 		}
 	}
 	fmt.Println("2d: ", twoD)
-	var expected [2][3]int{{1,2,3},{1,2,3}}
+	var expected = [2][3]int{{1, 2, 3}, {1, 2, 3}}
 	fmt.Print(expected)
 }
