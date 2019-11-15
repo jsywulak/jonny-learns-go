0eb61b12f196d5e3c65298fb8ee4ec774e561613
jonny sywulak <jonny@stelligent.com>
2019-11-15 15:08:02 -0500

    iterate 20191115150802

--- iterate.sh
+++ iterate.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-if go test
+if go test -v > output.txt
 then
     echo "tests passed, committing"
-    git add . && git commit -m "iterate `date +%Y%m%d%H%M%S`"
+    git add .
+    if git commit -m "iterate `date +%Y%m%d%H%M%S`"
+    then
+        rm -f last_failure.txt
+    else
+        cat last_failure.txt
+    fi
+    # if commit succeeeds, delete output
     git push -q &
 else
+    # save output
+    mv output.txt last_failure.txt
     echo "tests failed, reverting"
     git checkout .
 fi
