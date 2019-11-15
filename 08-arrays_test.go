b6c30f05e6365872041256c4870c844374cd3a0a
jonny sywulak <jonny@stelligent.com>
2019-11-15 14:15:23 -0500

    iterate 20191115141523

--- 08-arrays_test.go
+++ 08-arrays_test.go
@@ -13,9 +13,8 @@ func TestArrays(t *testing.T) {
 	assert.ElementsMatch(t, a, [5]int{0, 0, 0, 0, 0})
 
 	a[4] = 100
-	fmt.Println("set:", a)
 	fmt.Println("get:", a[4])
-
+	assert.Equal(t, 100, a[4])
 	fmt.Println("len:", len(a))
 
 	b := [5]int{1, 2, 3, 4, 5}
