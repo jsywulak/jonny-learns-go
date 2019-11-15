fb13fe3a5c3b11d9cfbdc3ff3d2b0992977468e8
jonny sywulak <jonny@stelligent.com>
2019-11-15 15:15:58 -0500

    iterate 20191115151558

--- 10-maps_test.go
+++ 10-maps_test.go
@@ -16,10 +16,9 @@ func TestMaps(t *testing.T) {
 	assert.Equal(t, len(map[string]int{"k1": 7, "k2": 13}), len(m))
 
 	assert.Equal(t, 7, m["k1"])
-	fmt.Println("len:", len(m))
-
+	assert.Equal(t, 2, len(m))
 	delete(m, "k2")
-	fmt.Println("map:", m)
+	assert.Equal(t, 1, len(m))
 
 	_, prs := m["k2"]
 	fmt.Println("prs:", prs)
