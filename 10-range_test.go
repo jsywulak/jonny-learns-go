226ff3a031f20e28a0061e6ebfdf4e8935b5b9c9
jonny sywulak <jonny@stelligent.com>
2019-11-15 15:21:20 -0500

    iterate 20191115152120

---
+++ 10-range_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func TestRanges() {
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+	fmt.Println(kvs)
+
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	for _, v := range kvs {
+		fmt.Println("val:", v)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+	for i, c := range "hello world" {
+		fmt.Println(i, c)
+	}
+}
