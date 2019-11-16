c6584007cf9cf7179042bfb4ce8a8993f1976010
jonny sywulak <jonny@stelligent.com>
2019-11-16 17:53:03 -0500

    iterate 20191116175303

--- 21-errors_test.go
+++ 21-errors_test.go
@@ -46,7 +46,7 @@ func TestErrors(t *testing.T) {
 	}
 	r2, e2 := f1(42)
 	if assert.Error(t, e2) {
-		// assert.Equal(t, e2, e *argError)
+		assert.Equal(t, e2, e&argError{0, ""})
 		fmt.Printf("%T\n", e2)
 	}
 	fmt.Println(r2, e2)
