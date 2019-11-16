ea034f299cb6019cdc98c5a8fd9fd2c00ca54a2a
jonny sywulak <jonny@stelligent.com>
2019-11-16 16:58:33 -0500

    iterate 20191116165833

--- 18-structs_test.go
+++ 18-structs_test.go
@@ -21,23 +21,18 @@ func NewPerson(name string) *person {
 func TestStructs(t *testing.T) {
 
 	bob := person{"Bob", 20}
-	fmt.Println(bob)
 	assert.Equal(t, bob.name, "Bob")
 	assert.Equal(t, bob.age, 20)
 	alice := person{name: "Alice", age: 30}
-	fmt.Println(alice)
 	assert.Equal(t, alice.name, "Alice")
 	assert.Equal(t, alice.age, 30)
 	fred := person{name: "Fred"}
-	fmt.Println(fred)
 	assert.Equal(t, fred.name, "Fred")
 	assert.Equal(t, fred.age, 0)
 	ann := &person{name: "Ann", age: 40}
 	assert.Equal(t, ann.name, "Ann")
 	assert.Equal(t, ann.age, 40)
-	fmt.Println(ann)
 	jon := NewPerson("Jon")
-	fmt.Println(jon)
 	assert.Equal(t, jon.name, "Jon")
 	assert.Equal(t, jon.age, 42)
 
