942be7ee4b18af693cffd1868df14dde6a5fbff2
jonny sywulak <jonny@stelligent.com>
2019-11-16 16:57:25 -0500

    iterate 20191116165725

--- 18-structs_test.go
+++ 18-structs_test.go
@@ -23,9 +23,11 @@ func TestStructs(t *testing.T) {
 	bob := person{"Bob", 20}
 	fmt.Println(bob)
 	assert.Equal(t, bob.name, "Bob")
-	assert.Equal(t, bob.age, "20")
+	assert.Equal(t, bob.age, 20)
 	alice := person{name: "Alice", age: 30}
 	fmt.Println(alice)
+	assert.Equal(t, alice.name, "Alice")
+	assert.Equal(t, alice.age, 30)
 	fred := person{name: "Fred"}
 	fmt.Println(fred)
 	ann := &person{name: "Ann", age: 40}
