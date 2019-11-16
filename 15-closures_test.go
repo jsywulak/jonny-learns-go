e79489e52b71799f2a5039ce9a449a6065cc09d3
jonny sywulak <jonny@stelligent.com>
2019-11-16 16:23:48 -0500

    iterate 20191116162348

--- 15-closures_test.go
+++ 15-closures_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,12 +21,9 @@ func TestClosures(t *testing.T) {
 	assert.Equal(t, 2, nextInt())
 	assert.Equal(t, 3, nextInt())
 	assert.Equal(t, 4, nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
-	fmt.Println(nextInt())
 
 	newInts := intSeq()
-	fmt.Println(newInts())
-	fmt.Println(newInts())
+	assert.Equal(t, 1, newInts())
+	assert.Equal(t, 2, newInts())
 
 }
