4405019a3f919b2602eee03d8f0ecee00388479c
jonny sywulak <jonny@stelligent.com>
2019-11-16 20:43:40 -0500

    iterate 20191116204340

--- 21-errors_test.go
+++ 21-errors_test.go
@@ -55,7 +55,7 @@ func TestErrors(t *testing.T) {
 		// assert.Equal(t, &errors.errorString{-1, "how does this work"}, e2)
 		// fmt.Printf("e2: %T\n", e2.(*argError))
 		fmt.Printf("e2: %T\n", e2)
-		fmt.Printf("e2: %T\n", given)
+		fmt.Printf("gi: %T\n", given)
 		fmt.Printf("ex: %T\n", expected)
 	}
 
