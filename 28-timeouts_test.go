f58745ceae6724d770936b1b67c5a78ac53957ca
jonny sywulak <jonny@stelligent.com>
2019-11-21 17:26:20 -0500

    iterate 20191121172620

--- 28-timeouts_test.go
+++ 28-timeouts_test.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTimeouts(t *testing.T) {
 	assert.Equal(t, 1, 1)
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "result 1"
+	}()
 }
