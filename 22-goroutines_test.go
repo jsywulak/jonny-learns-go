22ae823106d4e894007a71b3984d53c26c21e6f6
jonny sywulak <jonny@stelligent.com>
2019-11-21 14:43:19 -0500

    iterate 20191121144319

---
+++ 22-goroutines_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var dones = make(map[string]bool)
+var mutex = &sync.Mutex{}
+
+func f(from string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Second / 100)
+	}
+	mutex.Lock()
+	dones[from] = true
+	mutex.Unlock()
+}
+
+func TestRoutines(t *testing.T) {
+	go f("goroutine")
+	f("direct")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	assert.True(t, dones["goroutine"])
+	assert.True(t, dones["direct"])
+
+}
