85126a907e893ee2325c93e8dd23596dd3f7910a
jonny sywulak <jonny@stelligent.com>
2019-11-21 16:58:03 -0500

    iterate 20191121165803

--- 27-select_test.go
+++ 27-select_test.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSelect(t *testing.T) {
 	assert.Equal(t, 1, 1)
+	c1 := make(chan string)
+	c2 := make(chan string)
 
+	go func() {
+		time.Sleep(time.Second)
+		c1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
 }
