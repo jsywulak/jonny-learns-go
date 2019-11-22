563d42384c5978fc00b2dd345bbc27566033ed12
jonny sywulak <jonny@stelligent.com>
2019-11-22 14:50:02 -0500

    iterate 20191122145002

--- 34-worker-pools_test.go
+++ 34-worker-pools_test.go
@@ -6,6 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func poolworker(id int, jobs <-chan int, results chan<- int) {
+
+}
+
 func TestWorkerPools(t *testing.T) {
 	assert.Equal(t, 1, 1)
 
