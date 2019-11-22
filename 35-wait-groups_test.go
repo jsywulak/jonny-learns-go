4fb2bbaa575b08b0b969b170674a3554b779fbd7
jonny sywulak <jonny@stelligent.com>
2019-11-22 15:08:02 -0500

    iterate 20191122150802

--- 35-wait-groups_test.go
+++ 35-wait-groups_test.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func waitgroupworker(id int, wg *sync.WaitGroup) {
-	fmt.Printf("Worker %d starting\n", id)
-	time.Sleep(time.Second)
-	fmt.Printf("Worker %d complete\n", id)
+	// fmt.Printf("Worker %d starting\n", id)
+	// time.Sleep(time.Second)
+	// fmt.Printf("Worker %d complete\n", id)
 	wg.Done()
 }
 
