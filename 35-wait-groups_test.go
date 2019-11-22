80cd8eda65bc3d17e0b02d7ef1a78c536cb639dd
jonny sywulak <jonny@stelligent.com>
2019-11-22 15:04:21 -0500

    iterate 20191122150421

--- 35-wait-groups_test.go
+++ 35-wait-groups_test.go
@@ -7,7 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func waitgroupworker(id int, wg *sync.WaitGroup) {}
+func waitgroupworker(id int, wg *sync.WaitGroup) {
+
+	wg.Done()
+}
 
 func TestWaitGroups(t *testing.T) {
 	assert.Equal(t, 1, 1)
