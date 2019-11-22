9c85213fec8a8ce9174a26b98617890bf8c904ef
jonny sywulak <jonny@stelligent.com>
2019-11-22 14:28:17 -0500

    iterate 20191122142817

--- 32-timers_test.go
+++ 32-timers_test.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTimers(t *testing.T) {
 	assert.Equal(t, 1, 1)
+
+	timer1 := time.NewTimer(2 * time.Second)
+	<-timer1.C
 }
