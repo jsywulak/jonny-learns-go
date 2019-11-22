fd9b030954d15e34c71169dfcdf3f83d23e83979
jonny sywulak <jonny@stelligent.com>
2019-11-22 14:56:33 -0500

    iterate 20191122145633

--- 34-worker-pools_test.go
+++ 34-worker-pools_test.go
@@ -9,9 +9,9 @@ import (
 
 func poolworker(id int, jobs <-chan int, results chan<- int) {
 	for j := range jobs {
-		fmt.Println("worker", id, "started job", j)
+		// fmt.Println("worker", id, "started job", j)
 		// time.Sleep(time.Second)
-		fmt.Println("worker", id, "finished job", j)
+		// fmt.Println("worker", id, "finished job", j)
 		results <- j * 2
 	}
 
