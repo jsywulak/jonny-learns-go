cf372bd48a69dbdfafc8db79969146334f93b8a1
jonny sywulak <jonny@stelligent.com>
2019-11-22 14:36:18 -0500

    iterate 20191122143618

--- 32-tickers_test.go
+++ 32-tickers_test.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTickers(t *testing.T) {
 	assert.Equal(t, 1, 1)
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				fmt.Println("Ticket at ", t)
+			}
+		}
+	}()
 }
