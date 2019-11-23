c4e9a3ea4361439e6fe45ad63deddab8d1ac256a
jonny sywulak <jonny@stelligent.com>
2019-11-23 15:03:46 -0500

    iterate 20191123150346

--- 39-stateful-goroutines_test.go
+++ 39-stateful-goroutines_test.go
@@ -6,6 +6,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type ReadOp struct {
+	key int
+	resp chan int
+}
+
+type WriteOp {
+	key int
+	val int
+	resp chan bool
+}
+
 func TestStatefulGoRoutines(t *testing.T) {
 	assert.Equal(t, 1, 1)
+
+
 }
