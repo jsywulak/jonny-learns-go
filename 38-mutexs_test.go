a52d422aa7d4064f447e0c254c04e4fbee36e374
jonny sywulak <jonny@stelligent.com>
2019-11-23 14:31:52 -0500

    iterate 20191123143152

--- 38-mutexs_test.go
+++ 38-mutexs_test.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -17,7 +20,15 @@ func TestMutexes(t *testing.T) {
 
 	for r := 0; r < 100; r++ {
 		go func() {
-
+			total := 0
+			for {
+				key := rand.Intn(5)
+				mutex.Lock()
+				total += state[key]
+				mutex.Unlock()
+				atomic.AddUint64(&readOps, 1)
+				time.Sleep(time.Millisecond)
+			}
 		}()
 	}
 
