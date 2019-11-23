44a1b0de2db98a43083247b911243fd8365b12e1
jonny sywulak <jonny@stelligent.com>
2019-11-23 14:29:30 -0500

    iterate 20191123142930

--- 38-mutexs_test.go
+++ 38-mutexs_test.go
@@ -12,5 +12,9 @@ func TestMutexes(t *testing.T) {
 	assert.Equal(t, 1, 1)
 	var state = make(map[int]int)
 	var mutex = &sync.Mutex{}
-	fmt.Println(state, mutex)
+
+	var readOps uint64
+	var writeOps uint64
+
+	fmt.Println(state, mutex, readOps, writeOps)
 }
