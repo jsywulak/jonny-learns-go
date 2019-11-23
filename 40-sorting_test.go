feb314540519ecf13e69619ce8f3093b44d34fa0
jonny sywulak <jonny@stelligent.com>
2019-11-23 15:30:07 -0500

    iterate 20191123153007

--- 40-sorting_test.go
+++ 40-sorting_test.go
@@ -12,14 +12,19 @@ func TestSorting(t *testing.T) {
 	assert.Equal(t, 1, 1)
 
 	strs := []string{"c", "a", "b"}
+	ss := sort.StringsAreSorted(strs)
+	fmt.Println("Strings:", strs)
+	fmt.Println("sorted?", ss)
 	sort.Strings(strs)
 	fmt.Println("Strings:", strs)
 
 	ints := []int{7, 2, 4}
+	is := sort.IntsAreSorted(ints)
+	fmt.Println("sorted?", is)
 	sort.Ints(ints)
 	fmt.Println("ints:", ints)
-	ss := sort.StringsAreSorted(strs)
+	ss = sort.StringsAreSorted(strs)
 	fmt.Println("sorted?", ss)
-	is := sort.IntsAreSorted(ints)
+	is = sort.IntsAreSorted(ints)
 	fmt.Println("sorted?", is)
 }
