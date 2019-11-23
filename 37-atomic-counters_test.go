2269c8d3fd0790bd57a3a22e274dcd9c62e5bcc8
jonny sywulak <jonny@stelligent.com>
2019-11-23 14:19:17 -0500

    iterate 20191123141917

--- 37-atomic-counters_test.go
+++ 37-atomic-counters_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,4 +9,7 @@ import (
 
 func TestAtomicCounters(t *testing.T) {
 	assert.Equal(t, 1, 1)
+	var ops uint64
+	var wg sync.Waitgroup
+	fmt.Print(ops, wg)
 }
