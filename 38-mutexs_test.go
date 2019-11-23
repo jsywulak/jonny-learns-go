8bf9992e9dfd5b5aba56f504b6a589d222dc855b
jonny sywulak <jonny@stelligent.com>
2019-11-23 14:28:44 -0500

    iterate 20191123142844

--- 38-mutexs_test.go
+++ 38-mutexs_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,4 +9,6 @@ import (
 
 func TestMutexes(t *testing.T) {
 	assert.Equal(t, 1, 1)
+	var state = make(map[int]int)
+	fmt.Println(state)
 }
