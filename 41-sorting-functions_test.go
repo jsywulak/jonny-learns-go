4c17121a866b1929ddfb104d29b38e6f30e9ab6f
jonny sywulak <jonny@stelligent.com>
2019-12-03 15:04:23 -0500

    iterate 20191203150423

--- 41-sorting-functions_test.go
+++ 41-sorting-functions_test.go
@@ -12,6 +12,9 @@ func (s byLength) Len() int {
 	return len(s)
 }
 
+func (s byLength) Swap(i, j, swp) {
+	s[i], s[j] = s[j], s[i]
+}
 func TestSortingFunctions(t *testing.T) {
 	assert.Equal(t, 1, 1)
 }
