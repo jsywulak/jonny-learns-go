fd57ca469109e93c230612d7cdd4a0771600cb60
jonny sywulak <jonathan.sywulak@gmail.com>
2019-12-28 15:48:52 -0500

    iterate 20191228154852

--- 45-string-functions_test.go
+++ 45-string-functions_test.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var p = fmt.Println
+
 func TestStringFunctions(t *testing.T) {
 	assert.Equal(t, 1, 1)
+
+	p("Contains:", s.Contains("test", "es"))
+
 }
