6be76d3f4ed8819377b15842ef2a5244c018237a
jonny sywulak <jsywulak@gmail.com>
2019-12-28 15:22:52 -0500

    iterate 20191228152252

--- 44-collection-functions_test.go
+++ 44-collection-functions_test.go
@@ -43,5 +43,10 @@ func All(vs []string, f func(string) bool) bool {
 
 func Filter(vs []string, f func(string) bool) []string {
 	vsf := make([]string, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
 	return vsf
 }
