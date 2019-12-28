3ecf8460f47df97dbaed07e1011aed9034cbb730
jonny sywulak <jsywulak@gmail.com>
2019-12-28 14:23:25 -0500

    iterate 20191228142325

--- 42-defer_test.go
+++ 42-defer_test.go
@@ -25,3 +25,13 @@ func writeFile(f *os.File) {
 	fmt.Println("writing")
 	fmt.Fprintln(f, "data")
 }
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	err := f.Close()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
