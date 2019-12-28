d8535ebfdb288883ba1e41d9c3e979b0edad5891
jonny sywulak <jsywulak@gmail.com>
2019-12-28 15:07:30 -0500

    iterate 20191228150730

--- 42-defer_test.go
+++ 42-defer_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestDefer(t *testing.T) {
-	WrapperFunction(t)
+	WrapperFunctionToBeAbleToDefer(t)
 	dat, err := ioutil.ReadFile("/tmp/defer.txt")
 	if err != nil {
 		fmt.Println("you got bigger problems")
@@ -18,7 +18,7 @@ func TestDefer(t *testing.T) {
 	assert.Equal(t, "dataclosed", string(dat))
 }
 
-func WrapperFunction(t *testing.T) {
+func WrapperFunctionToBeAbleToDefer(t *testing.T) {
 	f := createFile("/tmp/defer.txt")
 	defer closeFile(f)
 	writeFile(f)
