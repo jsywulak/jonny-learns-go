5d20775d62698b97a0449bd8015dabdcee7c49a2
jonny sywulak <jsywulak@gmail.com>
2019-12-28 15:12:53 -0500

    iterate 20191228151252

--- 44-collection-functions_test.go
+++ 44-collection-functions_test.go
@@ -9,3 +9,12 @@ import (
 func TestCollectionFunctions(t *testing.T) {
 	assert.Equal(t, 1, 1)
 }
+
+func Index(vs []string, t string) int {
+	for i, v := range vs {
+		if v == t {
+			return i
+		}
+	}
+	return -1
+}
