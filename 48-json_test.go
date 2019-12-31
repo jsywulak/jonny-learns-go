c2e8a859aef03b7c6a3b87b50af98844004694ea
jonny sywulak <jonathan.sywulak@gmail.com>
2019-12-31 17:58:14 -0500

    iterate 20191231175814

--- 48-json_test.go
+++ 48-json_test.go
@@ -34,7 +34,6 @@ func TestJson(t *testing.T) {
 
 	slcD := []string{"apple", "peach", "pear"}
 	slcB, _ := json.Marshal(slcD)
-	fmt.Println(string(slcB))
 	assert.Equal(t, "[\"apple\",\"peach\",\"pear\"]", string(slcB))
 
 	mapD := map[string]int{"apple": 5, "lettuce": 7}
