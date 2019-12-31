1f5c6ecb9e187a789591bbb6eefaa3f344b146cb
jonny sywulak <jonathan.sywulak@gmail.com>
2019-12-31 18:14:24 -0500

    iterate 20191231181424

--- 48-json_test.go
+++ 48-json_test.go
@@ -60,7 +60,8 @@ func TestJson(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println(dat)
-	var exp map[string]interface{}
+	exp := make(map[string]interface{})
+
 	// exp["num"]=6.13
 	// exp["strs"]=[]string{"a", "b"}}
 	fmt.Println(exp)
