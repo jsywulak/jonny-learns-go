10967acfb9129629abbb7d7a4310bed8de7b74ab
jonny sywulak <jonathan.sywulak@gmail.com>
2019-12-31 17:27:23 -0500

    iterate 20191231172723

--- 48-json_test.go
+++ 48-json_test.go
@@ -10,6 +10,10 @@ type response1 struct {
 	Page   int
 	Fruits []string
 }
+type response2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruit"`
+}
 
 func TestJson(t *testing.T) {
 	assert.Equal(t, 1, 1)
