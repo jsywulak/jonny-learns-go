ef00cb808a512ef33117567764c7200d1c2c6084
jonny sywulak <jonathan.sywulak@gmail.com>
2019-12-31 17:21:59 -0500

    iterate 20191231172159

--- 47-regex_test.go
+++ 47-regex_test.go
@@ -21,9 +21,9 @@ func TestRegex(t *testing.T) {
 	assert.Equal(t, []int{0, 5, 1, 3}, r.FindStringSubmatchIndex("peach punch"))
 	assert.Equal(t, []string{"peach", "punch", "pinch"}, r.FindAllString("peach punch pinch", -1))
 	assert.Equal(t, [][]int{{0, 5, 1, 3}, {6, 11, 7, 9}, {12, 17, 13, 15}}, r.FindAllStringSubmatchIndex("peach punch pinch", -1))
-
 	assert.Equal(t, []string{"peach", "punch"}, r.FindAllString("peach punch pinch", 2))
-	fmt.Println(r.Match([]byte("peach")))
+
+	assert.True(t, r.Match([]byte("peach")))
 	r = regexp.MustCompile("p([a-z]+)ch")
 	fmt.Println(r)
 	fmt.Println(r.ReplaceAllString("a peach", "<fruit>"))
