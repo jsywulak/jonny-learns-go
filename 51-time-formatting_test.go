dbc54ef33442e889716a42d0ca3b48125c7cd10b
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 16:18:46 -0500

    iterate 20200101161846

--- 51-time-formatting_test.go
+++ 51-time-formatting_test.go
@@ -15,4 +15,6 @@ func TestTimeFormatting(t *testing.T) {
 	fmt.Println(now.Format(time.RFC3339))
 	t1, _ := time.Parse(time.RFC3339, "2012-11-01T22:08:41+00:00")
 	fmt.Println(t1)
+
+	fmt.Println(now.Format("3:04PM"))
 }
