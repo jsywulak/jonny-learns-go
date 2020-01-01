68afa2377135e9b908695335f38da459ed828895
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 16:41:25 -0500

    iterate 20200101164125

--- 51-time-formatting_test.go
+++ 51-time-formatting_test.go
@@ -9,17 +9,17 @@ import (
 )
 
 func TestTimeFormatting(t *testing.T) {
-	assert.Equal(t, 1, 1)
-
 	now := time.Date(
 		2020, 01, 01, 01, 01, 01, 651387237, time.UTC)
 	assert.Equal(t, "2020-01-01T01:01:01Z", now.Format(time.RFC3339))
+
 	t1, _ := time.Parse(time.RFC3339, "2012-11-01T22:08:41+00:00")
 	assert.Equal(t, "2012-11-01 22:08:41 +0000 +0000", t1.String())
 	assert.Equal(t, "1:01AM", now.Format("3:04PM"))
 	assert.Equal(t, "Wed Jan  1 01:01:01 2020", now.Format("Mon Jan _2 15:04:05 2006"))
 	assert.Equal(t, "Wed Jan  1 2020 01:01:01", now.Format("Mon Jan _2 2006 15:04:05"))
 	assert.Equal(t, "2020-01-01-T01:01:01.651387+00:00", now.Format("2006-01-02-T15:04:05.999999-07:00"))
+
 	form := "3 04 PM"
 	t2, _ := time.Parse(form, "8 41 PM")
 	assert.Equal(t, "0000-01-01 20:41:00 +0000 UTC", t2.String())
