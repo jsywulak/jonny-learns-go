913eaba81435bdfb3cb4405bf9ffa6aba792e267
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 16:15:57 -0500

    iterate 20200101161557

--- 51-time-formatting_test.go
+++ 51-time-formatting_test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTimeFormatting(t *testing.T) {
 	assert.Equal(t, 1, 1)
+
+	now := time.Now()
+	fmt.Println(now.Format(time.RFC3339))
 }
