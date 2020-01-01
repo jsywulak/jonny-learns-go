c8e7a5226dae7d330c21a6b3a5bd16b40583f124
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 17:07:34 -0500

    iterate 20200101170734

--- 52-random_test.go
+++ 52-random_test.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -20,22 +18,17 @@ func TestRandom(t *testing.T) {
 		assert.True(t, 0.0 <= randFloat2 && randFloat2 < 5.0)
 		randFloat3 := ((rand.Float64() * 5) + 5)
 		assert.True(t, 5.0 <= randFloat3 && randFloat3 < 10.0)
+		s2 := rand.NewSource(42)
+		r2 := rand.New(s2)
+		r2_1 := r2.Intn(100)
+		r2_2 := r2.Intn(100)
+
+		s3 := rand.NewSource(42)
+		r3 := rand.New(s3)
+		r3_1 := r3.Intn(100)
+		r3_2 := r3.Intn(100)
+		assert.Equal(t, r2_1, r3_1)
+		assert.Equal(t, r2_2, r3_2)
 	}
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-
-	fmt.Println(r1.Intn(100), r1.Intn(100))
-
-	s2 := rand.NewSource(42)
-	r2 := rand.New(s2)
-	r2_1 := r2.Intn(100)
-	r2_2 := r2.Intn(100)
-
-	s3 := rand.NewSource(42)
-	r3 := rand.New(s3)
-	r3_1 := r3.Intn(100)
-	r3_2 := r3.Intn(100)
-	assert.Equal(t, r2_1, r3_1)
-	assert.Equal(t, r2_2, r3_2)
 
 }
