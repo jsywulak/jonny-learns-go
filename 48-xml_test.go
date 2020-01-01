86da1d4e36df1cf4493759628506266c5764aa39
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 15:06:06 -0500

    iterate 20200101150606

--- 48-xml_test.go
+++ 48-xml_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/xml"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 type Plant struct {
+	XMLName xml.Name `xml:"plant"`
+	Id      int      `xml:"id,attr"`
 }
 
 func TestXml(t *testing.T) {
