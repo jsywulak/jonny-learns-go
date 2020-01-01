544d8d6cec8a0dbcb7fe95bd5be624ded4f8ae1b
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 15:20:14 -0500

    iterate 20200101152014

--- 48-xml_test.go
+++ 48-xml_test.go
@@ -28,11 +28,11 @@ func TestXml(t *testing.T) {
 	out, _ := xml.MarshalIndent(coffee, " ", "  ")
 	actual = string(out)
 	expected := ` <plant id="27">
-  <name>Coffee</name>
-  <origin>Ethiopia</origin>
-  <origin>Brazil</origin>
+   <name>Coffee</name>
+   <origin>Ethiopia</origin>
+   <origin>Brazil</origin>
  </plant>`
-	assert.NotNil(t, actual)
+	assert.Equal(t, expected, actual)
 	fmt.Println(string(out))
 	fmt.Println(expected)
 	fmt.Println(xml.Header + string(out))
