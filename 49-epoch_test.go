97fb73b9c7167780b53d33630a0ad57dce922906
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 16:06:57 -0500

    iterate 20200101160657

--- 49-epoch_test.go
+++ 49-epoch_test.go
@@ -19,9 +19,13 @@ func TestEpoch(t *testing.T) {
 	millis := nanos / 1000000
 
 	fmt.Println(now)
+	assert.Equal(t, "2020-01-01 01:01:01.651387237 +0000 UTC", now.String())
 	fmt.Println(secs)
+	assert.Equal(t, int64(1577840461), secs)
 	fmt.Println(millis)
+	assert.Equal(t, int64(1577840461651), millis)
 	fmt.Println(nanos)
+	assert.Equal(t, int64(1577840461651387237), nanos)
 
 	fmt.Println(time.Unix(secs, 0))
 	fmt.Println(time.Unix(0, nanos))
