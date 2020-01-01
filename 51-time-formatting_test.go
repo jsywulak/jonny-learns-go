1e430d399de8b95b8fb11b0897c6d69e8e479f6f
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 16:26:31 -0500

    iterate 20200101162631

--- 51-time-formatting_test.go
+++ 51-time-formatting_test.go
@@ -26,4 +26,7 @@ func TestTimeFormatting(t *testing.T) {
 
 	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
 
+	ansic := "Mon Jan _2 15:04:05 2006"
+	_, e := time.Parse(ansic, "8:41PM")
+	fmt.Println(e)
 }
