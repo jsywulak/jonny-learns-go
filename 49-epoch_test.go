180b4348d38b7a176dfefdcebe00e18ca2b7031b
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 16:09:37 -0500

    iterate 20200101160937

--- 49-epoch_test.go
+++ 49-epoch_test.go
@@ -24,5 +24,6 @@ func TestEpoch(t *testing.T) {
 	assert.Equal(t, int64(1577840461651387237), nanos)
 
 	fmt.Println(time.Unix(secs, 0))
+	assert.Equal(t, "2019-12-31 20:01:01 -0500 EST", time.Unix(secs, 0))
 	fmt.Println(time.Unix(0, nanos))
 }
