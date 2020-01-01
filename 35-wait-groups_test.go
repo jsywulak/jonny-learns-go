8dd0153bbd70a48ef1421eb4e50059df189b1ffd
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 16:43:42 -0500

    iterate 20200101164342

--- 35-wait-groups_test.go
+++ 35-wait-groups_test.go
@@ -23,6 +23,6 @@ func TestWaitGroups(t *testing.T) {
 		go waitgroupworker(i, &wg)
 	}
 	wg.Wait()
-	time.Sleep(time.Millisecond * 10)
+	time.Sleep(time.Millisecond * 50)
 	assert.Equal(t, 5, workercount, "should have five workers")
 }
