0df6cb9d80cd8e9842a6f91e061f6b48994d414b
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 15:49:55 -0500

    iterate 20200101154955

--- 49-time_test.go
+++ 49-time_test.go
@@ -33,7 +33,7 @@ func TestTime(t *testing.T) {
 
 	diff := now.Sub(then)
 	fmt.Println(diff)
-	fmt.Printf("%T\n", diff.String())
+	fmt.Printf("%T\n", diff)
 
 	fmt.Println(diff.Hours())
 	fmt.Printf("%T\n", diff.Hours())
