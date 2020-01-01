cccd217b0fdceb96dcebe237902c87d942831311
jonny sywulak <jonathan.sywulak@gmail.com>
2020-01-01 15:37:23 -0500

    iterate 20200101153723

--- 49-time_test.go
+++ 49-time_test.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTime(t *testing.T) {
 	assert.Equal(t, 1, 1)
+
+	now := time.Now()
+	fmt.Println(now)
 }
